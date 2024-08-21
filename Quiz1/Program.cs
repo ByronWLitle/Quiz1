6ab4270408bc22998f2512d505e919ab7481a6fc
ByronWLitle <bwlitle@amarillocollege.com>
2024-08-21 12:41:54 -0500

    Done

--- Quiz1/Program.cs
+++ Quiz1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace Quiz1
     {
         static void Main(string[] args)
         {
-            fdh
+            Console.WriteLine("This is Quiz 1.")
         }
     }
 }
